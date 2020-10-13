5c1bda99b5999d24eb51112683408e6711774c09
Eduardo Villamor <villamore838@gmail.com>
2020-10-13 10:33:38 -0500

    Final exercise

--- challenge-arrays.js
+++ challenge-arrays.js
@@ -1,7 +1,45 @@
 /*
+//Imprime el arreglo
+
 var names = ["Simon", "Natalia", "Nayib", "Diana", "Germán"]
 
-for (var i = 0; i < 5 /* o names.length*/; i++) {
+for (var i = 0; i < 5; i++) { //o names.length
     console.log(array[i])
 }
 */
+/*
+//Accediendo al elemento correcto
+
+var numbers = [1, 2, 3, 4, 5]
+var elemtOne = numbers[1]
+var elementTwo = numbers[3]
+
+var message = "Los elementos a imprimir son " + elemtOne + " y " + elementTwo
+console.log(message)
+*/
+/*
+//Agregando un último elemento
+
+var positions = ["Primero", "Segundo", "Tercero", "Cuarto"]
+positions.push("Soy el último elemento")
+
+for (i = 0; i < positions.length; i++) {
+    console.log(positions[i])
+}
+*/
+//Corrigiendo posiciones
+
+var arr = ["Primero", "Segundo", "Tercero", "Cuarto", "Soy el último elemento"]
+var newArray = ["Quinto", "Sexto"]
+var i = 0
+var elem = arr.splice(4, 1)[0]
+
+newArray.push(elem)
+
+for (i = 0; i < newArray.length; i++) {
+    arr.push(newArray[i])
+}
+
+for (i = 0; i < arr.length; i++) {
+    console.log(arr[i])
+}
