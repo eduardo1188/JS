eacf5ba7ad152217eb7e951deecd9d1769609cfc
Eduardo Villamor <villamore838@gmail.com>
2020-10-13 16:27:13 -0500

    Agregar .endswith

---
+++ strings-explicación.js
@@ -0,0 +1,9 @@
+// Recorrer los caracteres de una cadena usando ciclo
+/*var str = "Hola Mundo"
+
+for (var i = 0; i < str.length; i++) {
+    console.log(str[i])
+}*/
+var lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+console.log(lorem.endsWith("laborum.")
+)
