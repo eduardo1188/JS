2b2a0d0bc1105cd455bc932694750aee65e9eb80
Eduardo Villamor <villamore838@gmail.com>
2020-10-14 10:49:20 -0500

    finaliza edicion de objetos.js

--- objetos.js
+++ objetos.js
@@ -1,4 +1,19 @@
-let recipes = [
+// OBJETOS LIRERALES
+var persona = {
+  nombre: "Germán",
+  apellido: "Escobar",
+  edad: 35,
+  estatura: 1.8
+}
+persona["nombre"] //Tambien se pued eobtener el valor así
+console.log(persona.nombre); //para obtener el valor de un elem,ento dentro dle objeto
+console.log(persona.nombre); // imprime Germán
+console.log(persona.apellido); // imprime Escobar
+console.log(persona.edad); // imprime 35
+console.log(persona.estatura); // imprime 1.8
+
+
+/*let recipes = [
   {ingrediente: "leche en polvo", cantidad: "1 litro"},
   {ingrediente: "chocolate en barra", cantidad: "3 barras"},
   {ingrediente: "azúcar", cantidad: "30 gramos"}
@@ -8,3 +23,4 @@ for (let i = 0; i < recipes.length; i++) {
   console.log("ingrediente: " + recipe.ingrediente);
   console.log("cantidad: " + recipe.cantidad);
 }
+*/
